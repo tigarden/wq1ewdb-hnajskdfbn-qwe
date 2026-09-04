2044b07e7e9d5a44dfa190a957190509b1988316
tigarden <tigarden@users.noreply.github.com>
2026-09-05 01:52:03 +0300

    Updates

--- assets/vendor-react-CH66A7rh.js
+++ assets/vendor-react-CH66A7rh.js
@@ -1,4 +1,4 @@
-import{r as O}from"./vendor-icons-CTTd-6Ud.js";var y={exports:{}},n={};/**
+import{r as O}from"./vendor-icons-DE4uLfEq.js";var y={exports:{}},n={};/**
  * @license React
  * react-dom.production.js
  *
